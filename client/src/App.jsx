2fb2c3d2e8a4e14a0ab841a72dabf965bd4d7a57
Xander <xander.cage.botha@gmail.com>
2026-03-28 16:44:14 +0000

    feat: add protected routes for cart, checkout and orders

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar'
+import ProtectedRoute from './components/ProtectedRoute'
 import Home from './pages/Home'
 import Products from './pages/Products'
 import ProductDetail from './pages/ProductDetail'
@@ -19,12 +20,18 @@ function App() {
         <Route path="/products/:id" element={<ProductDetail />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/cart" element={<Cart />} />
-        <Route path="/checkout" element={<Checkout />} />
-        <Route path="/orders" element={<Orders />} />
+        <Route path="/cart" element={
+          <ProtectedRoute><Cart /></ProtectedRoute>
+        } />
+        <Route path="/checkout" element={
+          <ProtectedRoute><Checkout /></ProtectedRoute>
+        } />
+        <Route path="/orders" element={
+          <ProtectedRoute><Orders /></ProtectedRoute>
+        } />
       </Routes>
     </BrowserRouter>
   )
-};
+}
 
 export default App;
